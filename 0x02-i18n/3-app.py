565f765871f2da3f7f924d0e39c05209747d77fc
chisonia <sonya12slyvia@gmail.com>
2024-12-29 18:12:21 +0100

    Best lang

--- 0x02-i18n/3-app.py
+++ 0x02-i18n/3-app.py
@@ -1,35 +1,46 @@
 #!/usr/bin/env python3
-'''Best match module'''
-from flask import Flask, render_template
-from flask_babel import Babel, gettext as _
+"""A simple flask app
+"""
 
 
-class Config:
-    """Configuration class for the Flask app."""
-    LANGUAGES = ["en", "fr"]
-    BABEL_DEFAULT_LOCALE = "en"
-    BABEL_DEFAULT_TIMEZONE = "UTC"
+from flask import Flask, render_template, request
+from flask_babel import Babel
 
 
+class Config(object):
+    """_summary_
+
+    Returns:
+            _type_: _description_
+    """
+    LANGUAGES = ['en', 'fr']
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+
+# configure the flask app
 app = Flask(__name__)
 app.config.from_object(Config)
-
+app.url_map.strict_slashes = False
 babel = Babel(app)
 
 
 @babel.localeselector
 def get_locale():
-    """Determine the best match for supported languages."""
+    """_summary_
+
+    Returns:
+            _type_: _description_
+    """
     return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 
-@app.route("/")
+@app.route('/')
 def index():
-    """Route for the index page."""
-    return render_template("0-index.html",
-                           home_title=_("home_title"),
-                           home_header=_("home_header"))
+    """_summary_
+    """
+    return render_template('3-index.html')
 
 
-if __name__ == "__main__":
-    app.run(debug=True)
+if __name__ == '__main__':
+    app.run(port="5000", host="0.0.0.0", debug=True)
