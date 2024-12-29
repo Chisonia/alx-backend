6633343ba421801ee42e6b4f76a2822a88ca8331
chisonia <sonya12slyvia@gmail.com>
2024-12-29 18:09:49 +0100

    Best

---
+++ 0x02-i18n/3-app.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+'''Best match module'''
+from flask import Flask, render_template
+from flask_babel import Babel, gettext as _
+
+
+class Config:
+    """Configuration class for the Flask app."""
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app = Flask(__name__)
+app.config.from_object(Config)
+
+babel = Babel(app)
+
+
+@babel.localeselector
+def get_locale():
+    """Determine the best match for supported languages."""
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+@app.route("/")
+def index():
+    """Route for the index page."""
+    return render_template("0-index.html",
+                           home_title=_("home_title"),
+                           home_header=_("home_header"))
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
